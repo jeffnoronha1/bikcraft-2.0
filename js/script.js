4542727fa1af5217216ccd5b82ec310f6a860f1a
jeffnoronha777 <62403545+jeffnoronha777@users.noreply.github.com>
2022-02-23 15:40:51 -0300

    Adicionado plugin de animação no projeto

--- js/script.js
+++ js/script.js
@@ -24,3 +24,51 @@ function ativarProdutos(parametro) {
 }
 
 parametros.forEach(ativarProdutos)
+
+// Toggle perguntas frequentes
+const perguntas = document.querySelectorAll('.perguntas button')
+
+function ativarPergunta(event) {
+  const pergunta = event.currentTarget
+  const controls = pergunta.getAttribute('aria-controls')
+  const resposta = document.getElementById(controls)
+
+  //Adicionar e remover classe ativo
+  resposta.classList.toggle('ativo')
+  //Adicionar e remover atributo aria-expanded
+  if (resposta.classList.contains('ativo')) {
+    pergunta.setAttribute('aria-expanded', 'true')
+  } else {
+    pergunta.setAttribute('aria-expanded', 'false')
+  }
+}
+//Função para ativar o click na pergunta
+function togglePerguntas(pergunta) {
+  pergunta.addEventListener('click', ativarPergunta)
+}
+
+perguntas.forEach(togglePerguntas)
+
+// Galeria de imagens
+const galeria = document.querySelectorAll('.bicicleta-imagens img')
+const galeriaContainer = document.querySelector('.bicicleta-imagens')
+
+function trocarImagem(event) {
+  const img = event.currentTarget
+  // Faz a verificação da media query para desativar o efeito da geleria em telas menores
+  const media = matchMedia('(min-width: 1000px)').matches
+  if (media) {
+    galeriaContainer.prepend(img)
+  }
+}
+
+function eventosGaleria(img) {
+  img.addEventListener('click', trocarImagem)
+}
+
+galeria.forEach(eventosGaleria)
+
+// Animações
+if (window.SimpleAnime) {
+  new SimpleAnime()
+}
